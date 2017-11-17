42a4e99404aaa0b296643e2edc85dcb5a5c872a3
LENGYUANL <lengyuan2236@gmail.com>
2017-11-17 15:28:18 +0800

    Error:在主线程中调用了网络请求

---
+++ app/src/main/java/com/example/ll300/driple/models/Comments.java
@@ -0,0 +1,8 @@
+package com.example.ll300.driple.models;
+
+/**
+ * Created by ll300 on 2017/11/17.
+ */
+
+public class Comments {
+}
---
+++ app/src/main/java/com/example/ll300/driple/view/shot_detail/CommentViewHolder.java
@@ -0,0 +1,10 @@
+package com.example.ll300.driple.view.shot_detail;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ll300 on 2017/11/17.
+ */
+
+public class CommentViewHolder extends RecyclerView.ViewHolder {
+}
